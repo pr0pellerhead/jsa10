266822737f3ebc557d3868129b501f722d5275dd
Bojan Gavrovski <bojan@beyondbasics.co>
2021-04-21 22:06:46 +0200

    mailgun service implementation

---
+++ mailer/pkg/mailer/index.js
@@ -0,0 +1,30 @@
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const config = require('../config');
+
+const send = async (to, subject, message) => {
+    const mailgun = new Mailgun(formData);
+    const mg = mailgun.client({
+        username: config.get('mailer').username,
+        key: config.get('mailer').api_key
+    });
+
+    try {
+        let send = await mg.messages.create(
+            config.get('mailer').domain,
+            {
+                from: config.get('mailer').from,
+                to: to,
+                subject: subject,
+                text: message
+            }
+        );
+        console.log(send);
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+module.exports = {
+    send
+};
